43359cde3dc15e1b5f7af6a2803f82dbf0a9ab0e
DeivLex <peloliso.98@gmail.com>
2020-02-07 17:32:43 -0600

    Avances

---
+++ practica1/src/practica1/Practica1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica1;
+
+/**
+ *
+ * @author Davis
+ */
+public class Practica1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        home v = new home();
+        v.setTitle("Inicio");
+        v.setLocationRelativeTo(null);
+        v.show();
+        // TODO code application logic here
+    }
+    
+}
